3f8c5b56839008224299386a6143a0062a8b9636
kaitoii11 <kaitoii1111@gmail.com>
2019-06-30 10:07:58 +0900

    add calculation

--- public/keys.js
+++ public/keys.js
@@ -2,26 +2,59 @@ const calculator = document.getElementsByClassName('calculator')[0]
 const keys = calculator.getElementsByClassName('calculator_keys')[0]
 const display = calculator.getElementsByClassName('calculator_display')[0]
 
+const calculate = (val1, operator, val2) => {
+    let result = ''
+
+    if (operator === 'add'){
+        result = _add(val1, val2)
+    } else if (operator === 'subtract'){
+        result = _subtract(val1, val2)
+    } else if (operator === 'multiply'){
+        result = _multiply(val1, val2)
+    } else {
+        result = _divide(val1, val2)
+    }
+
+    return result
+}
+
+
 keys.addEventListener('click', e => {
     const key = e.target
     const action = key.dataset.action
+    const content = key.textContent
+    const disp = display.textContent
 
+    // number key
     if(!action) {
-        console.log('number')
+        console.log(content)
+        console.log(disp)
+        if(disp === '0' || calculator.dataset.previouskey === 'operator'){
+            display.textContent = content
+        } else {
+            display.textContent = disp + content
+            previouskey = content
+        }
     } else if (
         action === 'add' ||
         action === 'subtract' ||
         action === 'multiply' ||
         action === 'divide'
     ) {
-        console.log('operator')
+        //show pressed operator
+        key.classList.add('is-depressed')
+        calculator.dataset.previouskey = 'operator'
+        calculator.dataset.val1 = disp
+        calculator.dataset.operator = action
     } else if ( action === 'decimal') {
-        console.log('decimal')
     } else if ( action === 'clear') {
-        console.log('clear')
+        display.textContent = '0'
+
     } else if ( action === 'calculate') {
-        console.log('calculate')
+        const val1 = calculator.dataset.val1
+        const operator = calculator.dataset.operator
+        const val2 = disp
+
+        display.textContent = calculate(val1, operator, val2)
     }
-
-
 })
